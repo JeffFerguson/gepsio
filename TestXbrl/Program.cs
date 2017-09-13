709df56bb67251fdf423e66cb70b2d10785fe1f9
jnettleton <jnettleton@gmail.com>
2017-09-12 22:34:27 -0500

    add TestXbrl program, use "mmm-20161231.xsd" for testing

---
+++ TestXbrl/Program.cs
@@ -0,0 +1,13 @@
+﻿using JeffFerguson.Gepsio;
+
+namespace TestXbrl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xbrl = new XbrlDocument();
+            xbrl.Load("..\\..\\TestFiles\\mmm-20161231.xml");
+        }
+    }
+}
