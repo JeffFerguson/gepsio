010d706905d870b63d92ccf72c9e06e56927ae58
Jerry Nettleton <jnettleton@gmail.com>
2017-09-27 22:27:02 -0500

    add ibm for testing

--- TestXbrl/Program.cs
+++ TestXbrl/Program.cs
@@ -7,8 +7,9 @@ namespace TestXbrl
         static void Main(string[] args)
         {
             var xbrl = new XbrlDocument();
-            //var xbrlDoc = "..\\..\\TestFiles\\\mmm\\mmm-20161231.xml";
-            var xbrlDoc = "..\\..\\TestFiles\\dutch\\offentliggorelse.xml";
+            //var xbrlDoc = "..\\..\\TestFiles\\mmm\\mmm-20161231.xml";
+            //var xbrlDoc = "..\\..\\TestFiles\\dutch\\offentliggorelse.xml";
+            var xbrlDoc = "..\\..\\TestFiles\\ibm\\ibm-20170630.xml";
 
             xbrl.Load(xbrlDoc);
         }
