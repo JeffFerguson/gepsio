6701cbc338a7ac7e305502056288da1d3ca27ca9
Jeff Ferguson <gepsioxbrl@outlook.com>
2017-10-05 14:56:34 -0500

    Added loading examples.

---
+++ Examples/Load/Program.cs
@@ -0,0 +1,25 @@
+﻿using JeffFerguson.Gepsio;
+
+namespace Load
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadFromLocalFile();
+            LoadFromUrl();
+        }
+
+        static void LoadFromLocalFile()
+        {
+            var xbrlDoc = new XbrlDocument();
+            xbrlDoc.Load(@"..\..\JeffFerguson.Gepsio.Test\XBRL-CONF-2014-12-10\Common\300-instance\301-01-IdScopeValid.xml");
+        }
+
+        static void LoadFromUrl()
+        {
+            var xbrlDoc = new XbrlDocument();
+            xbrlDoc.Load("http://xbrlsite.com/US-GAAP/BasicExample/2010-09-30/abc-20101231.xml");
+        }
+    }
+}
