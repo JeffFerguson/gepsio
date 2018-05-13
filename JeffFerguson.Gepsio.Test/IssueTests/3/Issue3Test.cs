751ff4387f413acc4d3c4ef8304642db9d74afee
Jeff Ferguson <JeffFerguson@msn.com>
2018-05-13 09:47:15 -0500

    Unit test and XBRL input for Issue 3.

---
+++ JeffFerguson.Gepsio.Test/IssueTests/3/Issue3Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace JeffFerguson.Gepsio.Test.IssueTests._3
+{
+    /// <summary>
+    /// Issue 3 records a bug where XBRL using a Dutch taxonomy loaded into
+    /// Gepsio but returned no facts.
+    /// </summary>
+    [TestClass]
+    public class Issue3Test
+    {
+        [TestMethod]
+        public void VerifyFixForIssue3()
+        {
+            var xbrlDoc = new XbrlDocument();
+            var d = Directory.GetCurrentDirectory();
+            xbrlDoc.Load(@"..\..\..\IssueTests\3\offentliggorelse.xml");
+            Assert.IsTrue(xbrlDoc.XbrlFragments[0].Facts.Count > 0);
+        }
+    }
+}
