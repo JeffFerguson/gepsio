e5b88b7ddd4e2295ec81a8b616c8f08b0f41b1fc
Jeff Ferguson <gepsioxbrl@outlook.com>
2018-07-08 07:50:54 -0500

    Added PowerShell load example and VB.NET validity checks example.

---
+++ Examples/PowerShell/load.ps1
@@ -0,0 +1,17 @@
+﻿Add-Type -Path '..\..\JeffFerguson.Gepsio\bin\Debug\netstandard2.0\JeffFerguson.Gepsio.dll'
+
+function LoadFromLocalFile()
+{
+    $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+    $xbrlDoc = new-object "JeffFerguson.Gepsio.XbrlDocument"
+    $xbrlDoc.Load($ScriptDir + '..\..\..\JeffFerguson.Gepsio.Test\XBRL-CONF-2014-12-10\Common\300-instance\301-01-IdScopeValid.xml')
+}
+
+function LoadFromUrl()
+{
+    $xbrlDoc = new-object "JeffFerguson.Gepsio.XbrlDocument"
+    $xbrlDoc.Load('http://xbrlsite.com/US-GAAP/BasicExample/2010-09-30/abc-20101231.xml')
+}
+
+LoadFromLocalFile
+LoadFromUrl
